a8a4830e29f658ee43600b0c562e35c7a7ddd6fc
Dennis <dennis-farris@hotmail.com>
2025-07-28 17:28:04 -0400

    created callback function in task item for changing status, added comments to code

--- src/components/TaskList/TaskItem.tsx
+++ src/components/TaskList/TaskItem.tsx
@@ -1,27 +1,34 @@
 import type { TaskProps } from "../../types";
 
-export default function TaskItem({ task, onDelete, onEdit }: TaskProps) {
+export default function TaskItem({
+  task,
+  onDelete,
+  onEdit,
+  onStatusChange,
+}: TaskProps) {
+  //status list to create dropdown box
+  const status = ["Pending", "In Progress", "Overdue", "Completed"];
 
+  //create dropdown list for statuses
 
-    //status list to create dropdown box
-    const status=["Pending", "In Progress", "Overdue", "Completed"]
+  const statusDropdown = () => {
+    return status.map((stat) => {
+      return <option value={stat}>{stat}</option>;
+    });
+  };
 
-    //create dropdown list for statuses
-
-    const statusDropdown = () => {
-        return status.map((stat) => {
-            return <option value={stat}>{stat}</option>
-        })
-    }
-
-
-    //passes task id into callbakc functions for editing and deleting tasks
+  //passes task id into callback functions for changing status, handling
   const handleDelete = (taskId: number) => {
     onDelete(taskId);
   };
 
   const handleEdit = (taskId: number) => {
     onEdit(taskId);
+  };
+
+  //callback function for handling status
+  const handleChangeStatus = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    onStatusChange(task.id, event.target.value);
   };
 
   return (
@@ -31,8 +38,10 @@ export default function TaskItem({ task, onDelete, onEdit }: TaskProps) {
       <div>{task.priority}</div>
       <div>{task.dueDate.toLocaleDateString()}</div>
       <div>
-        <select value={task.status}>{statusDropdown()}</select>
-        </div>
+        <select value={task.status} onChange={handleChangeStatus}>
+          {statusDropdown()}
+        </select>
+      </div>
       <button onClick={() => handleEdit(task.id)}>Edit</button>
       <button onClick={() => handleDelete(task.id)}>Del</button>
     </li>
