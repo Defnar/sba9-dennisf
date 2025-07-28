8f2bee4abac6e83a4ae04b1d06988c7fdb34942c
Dennis <dennis-farris@hotmail.com>
2025-07-28 17:09:49 -0400

    TaskItem basic layout finished

--- src/components/TaskList/TaskItem.tsx
+++ src/components/TaskList/TaskItem.tsx
@@ -1,3 +1,26 @@
-export default function TaskItem() {
+import type { TaskProps } from "../../types";
+
+export default function TaskItem({task, onDelete, onEdit}: TaskProps) {
+
+    const handleDelete = (taskId: number)=> {
+        onDelete(taskId);
+    }
     
+    const handleEdit = (taskId: number) => {
+        onEdit(taskId);
+    }
+
+    return (
+        <li>
+            <div>{task.name}</div>
+            <div>{task.category}</div>
+            <div>{task.priority}</div>
+            <div>{task.dueDate.toLocaleDateString()}</div>
+            <div>{task.status}</div>
+            <button onClick={() => handleEdit(task.id)}>Edit</button>
+            <button onClick={() => handleDelete(task.id)}>X</button>
+        </li>
+    )
+    
+
 }
