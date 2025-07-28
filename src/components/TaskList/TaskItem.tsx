9c751095f888c6473a70ae90ad0f68517f557129
Dennis <dennis-farris@hotmail.com>
2025-07-28 17:24:23 -0400

    changed task item status to a dropdown box

--- src/components/TaskList/TaskItem.tsx
+++ src/components/TaskList/TaskItem.tsx
@@ -1,6 +1,21 @@
 import type { TaskProps } from "../../types";
 
 export default function TaskItem({ task, onDelete, onEdit }: TaskProps) {
+
+
+    //status list to create dropdown box
+    const status=["Pending", "In Progress", "Overdue", "Completed"]
+
+    //create dropdown list for statuses
+
+    const statusDropdown = () => {
+        return status.map((stat) => {
+            return <option value={stat}>{stat}</option>
+        })
+    }
+
+
+    //passes task id into callbakc functions for editing and deleting tasks
   const handleDelete = (taskId: number) => {
     onDelete(taskId);
   };
@@ -15,7 +30,9 @@ export default function TaskItem({ task, onDelete, onEdit }: TaskProps) {
       <div>{task.category}</div>
       <div>{task.priority}</div>
       <div>{task.dueDate.toLocaleDateString()}</div>
-      <div>{task.status}</div>
+      <div>
+        <select value={task.status}>{statusDropdown()}</select>
+        </div>
       <button onClick={() => handleEdit(task.id)}>Edit</button>
       <button onClick={() => handleDelete(task.id)}>Del</button>
     </li>
