4f4455f9123567dfb1dfc9a6fdb608ee21b256f9
Dennis <dennis-farris@hotmail.com>
2025-07-30 13:44:04 -0400

    added sort icons to the table

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,7 +1,18 @@
 import { useState } from "react";
-import type { Order, sortCategory, sortParameters, Task, TaskListProps } from "../../types";
+import type {
+  Order,
+  sortCategory,
+  sortParameters,
+  Task,
+  TaskListProps,
+} from "../../types";
 import { filterItems, searchTasks, sortTasks } from "../../utils/taskUtils";
 import TaskItem from "./TaskItem";
+import {
+  ArrowsUpDownIcon,
+  BarsArrowDownIcon,
+  BarsArrowUpIcon,
+} from "@heroicons/react/16/solid";
 
 export default function TaskList({
   tasks,
@@ -16,34 +27,47 @@ export default function TaskList({
     order: "None",
   });
 
+  //creates display list through 3 steps => filter => search => sort
   const displayList = (taskList: Task[]) => {
     const filteredList = filterItems(filters, taskList);
     const searchList = searchTasks(filteredList, search);
     return sortTasks(searchList, sort.category, sort.order);
   };
 
+  //handles sorting and sets sort state
   const handleSort = (sortCategory: sortCategory) => {
     let newOrder: Order;
-    if (sortCategory===sort.category) {
-    switch (sort.order) {
-      case "None":
-        newOrder = "Asc"
-        break;
-      case "Asc":
-        newOrder = "Desc"
-        break;
-      default:
-        newOrder = "None"
+    if (sortCategory === sort.category) {
+      switch (sort.order) {
+        case "None":
+          newOrder = "Asc";
+          break;
+        case "Asc":
+          newOrder = "Desc";
+          break;
+        default:
+          newOrder = "None";
+      }
+    } else {
+      newOrder = "Asc";
     }
-  }
-  else {
-    newOrder = "Asc"
-  }
-  setSort({
-    category: sortCategory,
-    order: newOrder
-  })
+    setSort({
+      category: sortCategory,
+      order: newOrder,
+    });
+  };
 
+  //sort icons
+  const sortIcon = (headerName: string) => {
+    if (headerName !== sort.category) return <ArrowsUpDownIcon className="w-4 h-4" />
+    switch (sort.order) {
+      case "Asc":
+        return <BarsArrowUpIcon className="w-4 h-4"/>;
+      case "Desc":
+        return <BarsArrowDownIcon className="w-4 h-4"/>;
+      default:
+        return <ArrowsUpDownIcon className="w-4 h-4"/>;
+    }
   }
 
   return (
@@ -51,11 +75,11 @@ export default function TaskList({
       <table>
         <thead>
           <tr>
-            <th onClick={() => handleSort("name")}>Name</th>
-            <th onClick={() => handleSort("category")}>Category</th>
-            <th onClick={() => handleSort("priority")}>Priority</th>
-            <th onClick={() => handleSort("dueDate")}>Due Date</th>
-            <th onClick={() => handleSort("status")}>Status</th>
+            <th onClick={() => handleSort("name")}>Name {sortIcon("name")}</th>
+            <th onClick={() => handleSort("category")}>Category {sortIcon("category")}</th>
+            <th onClick={() => handleSort("priority")}>Priority {sortIcon("priority")}</th>
+            <th onClick={() => handleSort("dueDate")}>Due Date {sortIcon("dueDate")}</th>
+            <th onClick={() => handleSort("status")}>Status {sortIcon("status")}</th>
             <th>Edit</th>
             <th>Del</th>
           </tr>
